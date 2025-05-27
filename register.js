e4498c74e1865e371de5f61adb1325c00bd6ca50
soynishappy <rlathdus0829@naver.com>
2025-05-27 21:40:15 +0900

    register.js

--- register.js
+++ register.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBu9OykAAv8Afwt68O3rImcd4kx0oNJj64",
   authDomain: "project1-47e8e.firebaseapp.com",
